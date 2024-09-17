852d940878d293b41af499ccb32d8061ff0f4dc3
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2024-09-17 17:10:53 +0545

    Created Header

---
+++ SourceCode/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<header>
+        <div class="logo">
+            <a href="home_page.html" class="logo"><img src="Photos/GamEase_transparent-.png" alt="Logo"></a>
+        </div>
+        <div class="nav-bar">
+                <form class="search-bar">
+                    <input type="text" placeholder="Search" name="search">
+                    <button type="submit"><img src="Photos/search.png" alt=""></button>
+                </form>
+            <ul>
+                <li><a href="home_page.html">Home</a></li>
+                <li><a href="#Games">Games</a></li>
+                <li><a href="about_us.html">About Us </a></li>
+                <li><a href="sign_in.html">Sign In</a></li>
+            </ul>
+        </div>
+    </header>
