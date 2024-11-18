e0dec2a1439214767880673100dd75b175b2289d
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2024-11-18 10:02:08 +0545

    edited footer.

--- SourceCode/footer.php
+++ SourceCode/footer.php
@@ -1,2 +1,29 @@
+<footer>
+    <div class="footer-container">
+    <div class="footer-logo">
+            <a href="index.php" class="logo"><img src="Photos/GamEase_transparent-.png" alt="Logo"></a>
+        </div>
+        <div class="footer-icons">
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-google-plus"></i></a>
+            <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+        </div>
+        <div class="footer-nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="games.php">Games</a></li>
+                <li><a href="about_us.php">About Us </a></li>
+                <li><a href="sign_in.php">Sign In</a></li>
+                <li><a href="our_team.php">Our Team</a></li>
+            </ul>
+        </div>
+        <div class="footer-end">
+            <p>All Copyright &copy; reserved by GamEase.</p>
+        </div>
+    </div>
+</footer>
+
 </body>
 </html>
