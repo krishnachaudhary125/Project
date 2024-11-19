dbc312fe3114cc6ce5aca6ad26f0d86e90a9a12f
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2024-11-19 08:08:54 +0545

    updated about us link

--- SourceCode/header.php
+++ SourceCode/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css" type="text/css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
 </head>
 <body>
 <header>
@@ -20,7 +20,7 @@
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="#">Games</a></li>
-                <li><a href="#">About Us </a></li>
+                <li><a href="about_us.php">About Us </a></li>
                 <li><a href="sign_in.php">Sign In</a></li>
             </ul>
         </div>
