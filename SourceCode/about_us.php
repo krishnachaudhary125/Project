cebd364cfdbec48d2a639eed241f1c99f602e9d4
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2024-11-19 07:45:51 +0545

    added about us page.

---
+++ SourceCode/about_us.php
@@ -0,0 +1,30 @@
+<?php 
+    include "header.php";
+?>
+
+    <div class="about-us">
+        <h1>About Us</h1>
+        <div class="about-us-main">
+            <img src="Photos/about_us_image1.png" alt="">
+            <div class="about-us-text">
+                <p>Welcome to GAMEASE, your ultimate destination for digital PC game keys. We are an online platform dedicated to offering gamers the best deals on authentic game keys, delivering the games you love at unbeatable prices.</p>
+            </div>
+        </div>
+        <div class="about-us-main">
+            <div class="about-us-text">
+                <p>Our goal is to make your gaming experience as smooth as possible. With instant digital delivery, secure transactions, and a user-friendly interface, purchasing game codes from GAMEASE is quick, easy, and reliable. We source all of our codes from trusted suppliers to ensure you get authentic and fully activated game keys.</p>
+            </div>
+            <img src="Photos/about_us_image2.png" alt="">
+        </div>
+        <div class="about-us-main">
+            <img src="Photos/about_us_image3.png" alt="">
+            <div class="about-us-text">
+            <p>Thank you for choosing GAMEASE is your one-stop destination for PC game keys. Start your next adventure today, and let the games begin.</p>
+        </div>
+    </div>
+    <h5>HAPPY GAMING!!</h5>
+    </div>
+
+<?php 
+    include "footer.php";
+?>
