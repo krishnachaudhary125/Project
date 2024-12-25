d6ad695d6b18fa1e5882c5b00dd89e6c856de2b4
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2024-12-25 19:47:31 +0545

    edited sign in page

--- SourceCode/sign_in.php
+++ SourceCode/sign_in.php
@@ -1,45 +1,38 @@
 <?php
 include "header.php";
-include "../Database/connection.php";
+include "./Database/connection.php";
 
 if (isset($_POST['submit']) == true) {
     $email = $_POST['email'];
     $psw = $_POST['psw'];
-    if ($email != '' && $psw != '') {
-        $sql = "SELECT id, fullname, email, password FROM users WHERE email='$email'";
-        $res = mysqli_query($conn, $sql);
-        $sqli = "SELECT id, email, password FROM admin WHERE email='$email'";
-        $result = mysqli_query($conn, $sqli);
+    $pswHash = sha1($psw);
 
-        if ($res->num_rows > 0) {
-            $pswHash = sha1($psw);
-            while ($row = mysqli_fetch_assoc($res)) {
-                // echo $pswHash . " + " . $row['password'];
-                if ($pswHash == $row['password']) {
-                    $_SESSION['fname'] = $row['fullname'];
-                    $_SESSION['login_status'] = true;
-                    $_SESSION['login_msg'] = "Login successfull.";
-                    header("location: ./index.php");
-                } else {
-                    $_SESSION['psw_err'] = "Password not matched.";
-                }
-            }
+    $sql = "
+        SELECT fullname, role FROM users WHERE email='$email' AND password='$pswHash'
+        UNION
+        SELECT fullname, 'admin' AS role FROM admin WHERE email='$email' AND password='$pswHash'
+    ";
+    
+    $result = mysqli_query($conn, $sql);
+
+    if (!$result) {
+        die("Query Failed: " . mysqli_error($conn)); 
+    }
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_array($result);
+
+        if ($row['role'] == 'admin') {
+            $_SESSION['role'] = 'admin';
+            $_SESSION['admin_name'] = $row['name'];
+            header('location: ./Admin/admin_panel.php?dashboard');
+        } elseif ($row['role'] == 'user') {
+            $_SESSION['role'] = 'user';
+            $_SESSION['user_name'] = $row['name'];
+            header('location:index.php');
         }
-        if ($result->num_rows > 0) {
-            $pswHash = sha1($psw);
-            while ($row = mysqli_fetch_assoc($result)) {
-                // echo $pswHash . " + " . $row['password'];
-                if ($pswHash == $row['password']) {
-                    $_SESSION['login_status'] = true;
-                    $_SESSION['login_msg'] = "Login successfull.";
-                    header("location: ./Admin/admin_panel.php?dashboard");
-                } else {
-                    $_SESSION['psw_err'] = "Password not matched.";
-                }
-            }
-        } else {
-            $_SESSION['user_err'] = "E-Mail not matched!";
-        }
+    } else {
+        $error[] = 'Incorrect email or password!';
     }
 }
 ?>
@@ -47,13 +40,18 @@ if (isset($_POST['submit']) == true) {
     <form action="#" method="post" class="sign-in">
         <h1>Sign In</h1>
         <div class="sign-in-form">
+            <?php
+            if (isset($error)) {
+                foreach ($error as $error) {
+                    echo '<span class="error-msg">' . $error . '</span>';
+                }
+            }
+            ?>
             <div class="sign-in-field">
                 <input type="email" id="email" name="email" value="" placeholder="Email Address">
-                <span><?php echo isset($_SESSION['user_err']) ? $_SESSION['user_err'] : ''; ?></span>
             </div>
             <div class="sign-in-field">
                 <input type="password" id="psw" name="psw" value="" placeholder="Password">
-                <span><?php echo isset($_SESSION['psw_err']) ? $_SESSION['psw_err'] : ''; ?></span>
             </div>
             <button type="submit" id="submit" name="submit">SIGN IN</button>
         </div>
