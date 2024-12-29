25810d1f24d59af13a512ce5351004d5f8551b3f
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2024-12-29 19:43:14 +0545

    working category button on game

--- Function/game_function.php
+++ Function/game_function.php
@@ -52,7 +52,7 @@ function gamesByCategory(){
     global $conn;
     if(isset($_GET['category_id'])){
         $category_id=$_GET['category_id'];
-$select_game = "SELECT * FROM games INNER JOIN category ON games.category=category.category_id where games.category=$category_id";
+$select_game = "SELECT * FROM games INNER JOIN category ON games.category_id=category.category_id where games.category_id=$category_id";
 $game_select = mysqli_query($conn, $select_game);
 while ($row_data = mysqli_fetch_assoc($game_select)):
 ?>
@@ -60,12 +60,12 @@ while ($row_data = mysqli_fetch_assoc($game_select)):
     <div class="game-body">
         <div class="media-container">
             <?php
-                if (!empty($row_data['photo'])) {
-                    echo '<img src="../Database/uploads/photos/' . ($row_data['photo']) . '" alt="Game Photo" class="image">';
+                if (!empty($row_data['game_photo'])) {
+                    echo '<img src="' . $row_data['game_photo'] . '" alt="Game Photo" class="image">';
                 }
                 ?>
             <video class="video" loop muted>
-                <source src="<?php echo '../Database/uploads/videos/' . $row_data['video']; ?>" type="video/mp4" />
+                <source src="<?php echo $row_data['game_video']; ?>" type="video/mp4" />
             </video>
         </div>
     </div>
@@ -75,9 +75,14 @@ while ($row_data = mysqli_fetch_assoc($game_select)):
         </div>
         <div class="game-description">
             <p><?php echo $row_data['description']; ?></p>
-            <p>Release Date : <?php echo $row_data['release_date']; ?></p>
-            <p>Developer : <?php echo $row_data['developer']; ?></p>
-            <p>Category : <?php echo $row_data['category']; ?></p>
+            <p>Release Date : <?php
+                            if (!empty($row_data['release_date'])) {
+                                $formattedDate = date("M d, Y", strtotime($row_data['release_date']));
+                                echo $formattedDate;
+                            }
+                            ?></p>
+            <p>Developer : <?php echo $row_data['game_developer']; ?></p>
+            <p>Category : <?php echo $row_data['category_name']; ?></p>
         </div>
         <div class="game-cart">
             <div class="cost"><?php echo 'Npr  ' . $row_data['game_price']; ?></div>
